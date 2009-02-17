bcc0e9da9a482106530e612f6c153115a435f60a
jbrooke <>
2009-02-17 12:08:07 +0000

    update sample size

--- Simulation/test/stoppedHSCP_300_200.py
+++ Simulation/test/stoppedHSCP_300_200.py
@@ -1,7 +1,7 @@
 
 from StoppedHSCP.Simulation.stoppedHSCPSim import *
 
-process.maxEvents.input = 100
+process.maxEvents.input = 10000
 process.source.PythiaParameters.SLHAParameters = cms.vstring("SLHAFILE = \'StoppedHSCP/Simulation/data/Rhadron_1jet_300_200.dat\'")
 process.VtxSmeared.stoppedData = cms.FileInPath('StoppedHSCP/Simulation/data/stopped_rhadrons_gluino300.txt')
 
