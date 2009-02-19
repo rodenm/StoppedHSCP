1bac0fbd5ee3497c34bda8e67162d4cd89a4d670
fedor <>
2009-02-19 15:01:09 +0000

    Stage 1, stage 2 simulation stuff

---
+++ AddOn/plugins/SealModule.cc
@@ -0,0 +1,8 @@
+#include "RHStopTracer.h"
+
+#include "FWCore/PluginManager/interface/ModuleDef.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+#include "SimG4Core/Watcher/interface/SimWatcherFactory.h"
+
+DEFINE_SEAL_MODULE ();
+DEFINE_SIMWATCHER(RHStopTracer);
