55c8646246ab1225366b03c8169df0b0e6c4381b
fedor <>
2009-02-19 15:25:21 +0000

    Simulation stage1 and some stage2 stuff

---
+++ AddOn/plugins/RHDecay.h
@@ -0,0 +1,23 @@
+#include "IOMC/EventVertexGenerators/interface/BaseEvtVtxGenerator.h"
+
+#include <fstream>
+
+class RHDecay : public BaseEvtVtxGenerator 
+{
+public:
+  RHDecay(const edm::ParameterSet & p);
+  virtual ~RHDecay();
+
+  /// return a new event vertex
+  //virtual CLHEP::Hep3Vector* newVertex();
+  virtual HepMC::FourVector* newVertex() ;
+  
+  virtual TMatrixD* GetInvLorentzBoost() {
+    return 0;
+  }
+private:
+  std::string mFileName;
+  std::ifstream* mFile;
+  double mTimeMin;
+  double mTimeMax;
+};
--- AddOn/plugins/SealModule.cc
+++ AddOn/plugins/SealModule.cc
@@ -1,8 +1,11 @@
-#include "RHStopTracer.h"
 
 #include "FWCore/PluginManager/interface/ModuleDef.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
-#include "SimG4Core/Watcher/interface/SimWatcherFactory.h"
-
 DEFINE_SEAL_MODULE ();
+
+#include "SimG4Core/Watcher/interface/SimWatcherFactory.h"
+#include "RHStopTracer.h"
 DEFINE_SIMWATCHER(RHStopTracer);
+
+#include "RHDecay.h"
+DEFINE_ANOTHER_FWK_MODULE(RHDecay);
