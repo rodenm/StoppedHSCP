852b0bf250e9e06c67a6507d5acff165421cb642
jbrooke <>
2009-10-13 20:15:38 +0000

    analyzers from Ken

---
+++ Analysis/src/HcalTiming.cc
@@ -0,0 +1,214 @@
+// -*- C++ -*-
+//
+// Package:    HcalTiming
+// Class:      HcalTiming
+// 
+
+//
+// Original Author:  Kenneth Case Rossato
+//         Created:  Fri Oct 17 16:17:21 CEST 2008
+// $Id: HcalChannelMon.cc,v 1.2 2008/11/07 15:04:59 rossato Exp $
+// Copied and became HcalTiming.cc 2009/01/21
+// 
+
+
+// system include files
+#include <memory>
+
+// user include files
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/EDAnalyzer.h"
+
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+
+#include "FWCore/ServiceRegistry/interface/Service.h"
+#include "FWCore/Framework/interface/ESHandle.h"
+#include "FWCore/Framework/interface/EventSetup.h"
+
+#include "PhysicsTools/UtilAlgos/interface/TFileService.h"
+#include "DataFormats/HcalDigi/interface/HcalDigiCollections.h"
+
+#include "TH1D.h"
+#include "TH2D.h"
+
+//
+// class decleration
+//
+
+class HcalTiming : public edm::EDAnalyzer {
+   public:
+      explicit HcalTiming(const edm::ParameterSet&);
+      ~HcalTiming();
+
+
+   private:
+      virtual void beginJob(const edm::EventSetup&) ;
+      virtual void analyze(const edm::Event&, const edm::EventSetup&);
+      virtual void endJob() ;
+
+  //  unsigned int getFibNOrbMsgBCN(const HcalHTRData &, int);
+      // ----------member data ---------------------------
+
+  edm::Service<TFileService> fs;
+
+  TH1D *leading_digi;
+  TH1D *p0_to_p1;
+  TH1D *p1_to_p2;
+  TH2D *p0p1_vs_p1p2;
+  TH1D *peak_location;
+  TH1D *weighted_peak_location;
+  TH1D *weighted_peak_location_2;
+  TH2D *p0p1_vs_loc;
+  TH1D *p0p1_to_total;
+  TH1D *lumi_segments;
+
+};
+
+//
+// constants, enums and typedefs
+//
+
+//
+// static data member definitions
+//
+
+//
+// constructors and destructor
+//
+HcalTiming::HcalTiming(const edm::ParameterSet& iConfig)
+{
+   //now do what ever initialization is needed
+
+
+}
+
+
+HcalTiming::~HcalTiming()
+{
+ 
+   // do anything here that needs to be done at desctruction time
+   // (e.g. close files, deallocate resources etc.)
+
+}
+
+
+//
+// member functions
+//
+
+/*
+unsigned int HcalTiming::getFibNOrbMsgBCN(const HcalHTRData &htr, int n) {
+
+  switch (n) {
+  case 1:
+    return htr.getFib1OrbMsgBCN(); break;
+  case 2:
+    return htr.getFib2OrbMsgBCN(); break;
+  case 3:
+    return htr.getFib3OrbMsgBCN(); break;
+  case 4:
+    return htr.getFib4OrbMsgBCN(); break;
+  case 5:
+    return htr.getFib5OrbMsgBCN(); break;
+  case 6:
+    return htr.getFib6OrbMsgBCN(); break;
+  case 7:
+    return htr.getFib7OrbMsgBCN(); break;
+  case 8:
+    return htr.getFib8OrbMsgBCN(); break;
+  }
+
+  throw std::range_error("Fiber number not 1-8");
+  return 0;
+}
+*/
+
+// ------------ method called to for each event  ------------
+void
+HcalTiming::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
+{
+   using namespace edm;
+
+   Handle<HBHEDigiCollection> digis;
+   iEvent.getByType(digis);
+   double leading_total = 0;
+   HBHEDigiCollection::const_iterator leading_index;
+
+   for (HBHEDigiCollection::const_iterator cit = digis->begin();
+	cit != digis->end(); cit++) {
+     
+     double running_total = 0;
+     for (int i = 0; i < cit->size(); i++)
+       running_total += cit->sample(i).nominal_fC();
+
+     if (running_total > leading_total) {
+       leading_total = running_total;
+       leading_index = cit;
+     }
+   }
+
+   int peak = 0;
+   double p0 = 0;
+   double total = 0;
+   for (int i = 0; i < leading_index->size(); i++) {
+     if (leading_index->sample(i).nominal_fC() > p0) {
+       peak = i; p0 = leading_index->sample(i).nominal_fC();
+     }
+     leading_digi->Fill(i, leading_index->sample(i).nominal_fC());
+     if (i != 0 && i != 9)
+       total += leading_index->sample(i).nominal_fC();
+   }
+   double p1 = 0;
+   double p2 = 0;
+   if (peak < leading_index->size() - 1) {
+     p1 = leading_index->sample(peak + 1).nominal_fC();
+     p0_to_p1->Fill(p0 / p1);
+     p0p1_to_total->Fill((p0 + p1)/total);
+     p0p1_vs_loc->Fill(peak, p0 / p1);
+   }
+   if (peak < leading_index->size() - 2) {
+     p2 = leading_index->sample(peak + 2).nominal_fC();
+     p1_to_p2->Fill(p1 / p2);
+     p0p1_vs_p1p2->Fill(p0 / p1, p1 / p2);
+   }
+   peak_location->Fill(peak);
+   weighted_peak_location->Fill(peak, p0);
+   weighted_peak_location_2->Fill(peak, p0*p0);
+
+   lumi_segments->Fill(iEvent.luminosityBlock());
+}
+
+
+// ------------ method called once each job just before starting event loop  ------------
+void 
+HcalTiming::beginJob(const edm::EventSetup&iSetup)
+{    
+
+    leading_digi = fs->make<TH1D>("leading_digi", "ADCs for the Leading Digi",
+				  10, -.5, 9.5);
+    p0_to_p1 = fs->make<TH1D>("p0_to_p1", "Peak to peak-plus-one",
+			      100, 0, 20);
+    p1_to_p2 = fs->make<TH1D>("p1_to_p2", "Peak-plus-one to peak-plus-two",
+			      100, 0, 20);
+    p0p1_vs_p1p2 = fs->make<TH2D>("p0p1_vs_p1p2", "P0-P1-P2 discrimination",
+				  100, 0, 20, 100, 0, 20);
+    peak_location = fs->make<TH1D>("peak_location", "Location of the peak of the leading digi", 10, -.5, 9.5);
+    p0p1_vs_loc = fs->make<TH2D>("p0p1_vs_loc", "Peak-to-plus-one ratio against Peak location", 10, -.5, 9.5, 100, 0, 20);
+    weighted_peak_location = fs->make<TH1D>("weighted_peak_location", "Peak Location weighted by peak height", 10, -.5, 9.5);
+    weighted_peak_location_2 = fs->make<TH1D>("weighted_peak_location_2", "Peak Location weighted by peak height^2", 10, -.5, 9.5);
+
+    p0p1_to_total = fs->make<TH1D>("p0p1_to_total", "Peak and neighbor over all-but-0/9",
+				   20, 0, 1);
+    lumi_segments = fs->make<TH1D>("lumi_segments", "Occupancy per Lumi Section", 100, 0, 100);
+}
+
+// ------------ method called once each job just after ending the event loop  ------------
+void 
+HcalTiming::endJob() {
+}
+
+//define this as a plug-in
+DEFINE_FWK_MODULE(HcalTiming);
---
+++ Analysis/src/JetAnalyzer.cc
@@ -0,0 +1,173 @@
+// -*- C++ -*-
+//
+// Package:    JetAnalyzer
+// Class:      JetAnalyzer
+// 
+/**\class JetAnalyzer JetAnalyzer.cc UserCode/JetAnalyzer/src/JetAnalyzer.cc
+
+ Description: <one line class summary>
+
+ Implementation:
+     <Notes on implementation>
+*/
+//
+// Original Author:  "Kenneth Rossato"
+//         Created:  Fri Oct  2 12:11:33 CDT 2009
+// $Id$
+//
+//
+
+
+// system include files
+#include <memory>
+
+// user include files
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/EDAnalyzer.h"
+
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "DataFormats/JetReco/interface/CaloJetCollection.h"
+
+#include "FWCore/ServiceRegistry/interface/Service.h"
+#include "PhysicsTools/UtilAlgos/interface/TFileService.h"
+
+#include "TH1D.h"
+#include "TH2D.h"
+
+//
+// class decleration
+//
+
+class JetAnalyzer : public edm::EDAnalyzer {
+   public:
+      explicit JetAnalyzer(const edm::ParameterSet&);
+      ~JetAnalyzer();
+
+
+   private:
+      virtual void beginJob() ;
+      virtual void analyze(const edm::Event&, const edm::EventSetup&);
+      virtual void endJob() ;
+
+      // ----------member data ---------------------------
+  edm::Service<TFileService> fs;
+  edm::InputTag jets;
+  TH1D *e, *e2, *eta, *phi, *n60, *n90, *ls;
+  TH2D *eEta;
+};
+
+//
+// constants, enums and typedefs
+//
+
+//
+// static data member definitions
+//
+
+//
+// constructors and destructor
+//
+JetAnalyzer::JetAnalyzer(const edm::ParameterSet& iConfig)
+  : jets (iConfig.getParameter<edm::InputTag>("jets"))
+{
+   //now do what ever initialization is needed
+
+}
+
+
+JetAnalyzer::~JetAnalyzer()
+{
+ 
+   // do anything here that needs to be done at desctruction time
+   // (e.g. close files, deallocate resources etc.)
+
+}
+
+
+//
+// member functions
+//
+
+// ------------ method called to for each event  ------------
+void
+JetAnalyzer::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
+{
+   using namespace edm;
+
+   ls->Fill(iEvent.luminosityBlock());
+
+   Handle<reco::CaloJetCollection> pIn;
+   iEvent.getByLabel(jets,pIn);
+
+   reco::CaloJetCollection::const_iterator leading, current;
+   double highestE = 0;
+   double highestEt = 0;
+   for (leading = current = pIn->begin(); current != pIn->end(); current++) {
+     if (current->et() > highestEt &&
+	 current->energy() > 1 &&
+	 fabs(current->eta() < 3)
+	 ) {
+       highestE = current->energy();
+       highestEt = current->et();
+       leading = current;
+     }
+   }
+   if (highestE == 0) {
+     e->Fill(0.);
+     e2->Fill(0.);
+     eta->Fill(0.);
+     phi->Fill(0.);
+     n60->Fill(0.);
+     n90->Fill(0.);
+     eEta->Fill(0.,0.);
+     return;
+   }
+
+   e->Fill(leading->energy());
+   e2->Fill(leading->energy());
+   eta->Fill(leading->eta());
+   phi->Fill(leading->phi());
+   n60->Fill(leading->n60());
+   n90->Fill(leading->n90());
+   eEta->Fill(leading->energy(), leading->eta());
+   
+#ifdef THIS_IS_AN_EVENTSETUP_EXAMPLE
+   ESHandle<SetupData> pSetup;
+   iSetup.get<SetupRecord>().get(pSetup);
+#endif
+
+
+
+   
+
+}
+
+
+// ------------ method called once each job just before starting event loop  ------------
+void 
+JetAnalyzer::beginJob()
+{
+  e = fs->make<TH1D>("e", "Leading Jet Energy", 100, 0, 1000);
+  e2 = fs->make<TH1D>("e2", "Leading Jet Energy", 100, 0, 200);
+  eta = fs->make<TH1D>("eta", "Leading Jet Eta", 70, -3.5, 3.5);
+  phi = fs->make<TH1D>("phi", "Leading Jet Phi", 60, -3.4151, 3.4151);
+  n60 = fs->make<TH1D>("n60", "Leading Jet n60", 50, 0, 50);
+  n90 = fs->make<TH1D>("n90", "Leading Jet n90", 50, 0, 50);
+
+  ls = fs->make<TH1D>("ls", "Lumi Section", 200, 0, 200);
+
+  eEta = fs->make<TH2D>("eEta", "Leading Jet E v. Eta",
+			100, 0, 1000,
+			70, -3.5, 3.5);
+}
+
+// ------------ method called once each job just after ending the event loop  ------------
+void 
+JetAnalyzer::endJob() {
+}
+
+//define this as a plug-in
+DEFINE_FWK_MODULE(JetAnalyzer);
