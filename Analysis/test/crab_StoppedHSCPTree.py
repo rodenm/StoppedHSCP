9e95346d829f444c02440b4e7b5b9ae95ba80cce
jbrooke <>
2009-11-23 15:17:44 +0000

    v13

--- Analysis/test/crab_StoppedHSCPTree.py
+++ Analysis/test/crab_StoppedHSCPTree.py
@@ -18,7 +18,7 @@ scheduler = glitecoll
 ### The data you want to access (to be found on DBS) 
 # CRAFT 09
 datasetpath=/Calo/CRAFT09-GR09_31X_V5P_StoppedHSCP-332_v4/RAW-RECO
-runselection=111039
+runselection=110958,110972,110987,110998,111009,111039,111138
 
 # CRAFT 08
 #datasetpath=/Calo/Commissioning08-v1/RAW
@@ -46,9 +46,9 @@ copy_data = 1
 storage_element          = lcgse02.phy.bris.ac.uk
 storage_path             = /srm/managerv2?SFN=/cms
 storage_port             = 8444
-user_remote_dir          = /store/user/jbrooke/StoppedHSCP_CRAFT09_v11
+user_remote_dir          = /store/user/jbrooke/StoppedHSCP_CRAFT09_v13
 
-ui_working_dir = StoppedHSCP_CRAFT09_v11
+ui_working_dir = StoppedHSCP_CRAFT09_v13
 
 #if server 
 thresholdLevel = 50
--- Analysis/test/stoppedHSCPTree.py
+++ Analysis/test/stoppedHSCPTree.py
@@ -68,7 +68,7 @@ process.TFileService = cms.Service("TFileService",
 # Bens ntuple producer
 process.load('StoppedHSCP/Analysis/globalRunAnalyser_cfi')
 #process.globalRunAnalyser.IncludeJetDetails=cms.untracked.bool(True)
-process.globalRunAnalyser.IncludeDigisOld=cms.untracked.bool(False)
+process.globalRunAnalyser.IncludeDigisOld=cms.untracked.bool(True)
 #process.globalRunAnalyser.IncludeCaloTowers=cms.untracked.bool(True)
 #process.globalRunAnalyser.IncludeRecHits=cms.untracked.bool(True)                               
 
@@ -92,13 +92,13 @@ process.myPath = cms.Path(
 #    +process.reconstructionCosmics
 
 # CRAFT 09 reproduce missing RECO info
-#    +process.hcalDigis
-#    +process.gtDigis
-#    +process.gctDigis
-#    +process.l1extraParticles
+    process.hcalDigis
+    +process.gtDigis
+    +process.gctDigis
+    +process.l1extraParticles
 
 # fake HLT selection
-    process.caloFilter
+    +process.caloFilter
 
 # Ken's histograms
     +process.jetanalyzer
@@ -115,48 +115,48 @@ process.schedule = cms.Schedule(
     process.myPath
 )
 
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(1000) )
 readFiles = cms.untracked.vstring()
 secFiles = cms.untracked.vstring() 
 process.source = cms.Source ("PoolSource",fileNames = readFiles, secondaryFileNames = secFiles)
 
 # CRAFT 09 skim
-#readFiles.extend( [
-#    '/store/data/CRAFT09/Calo/RAW-RECO/GR09_31X_V5P_StoppedHSCP-332_v4/0022/FC9ECC6F-2299-DE11-BA40-0018F3D09636.root',
-#    '/store/data/CRAFT09/Calo/RAW-RECO/GR09_31X_V5P_StoppedHSCP-332_v4/0022/FC6A5AAA-CA98-DE11-A0F2-001A92810AE0.root',
-#    '/store/data/CRAFT09/Calo/RAW-RECO/GR09_31X_V5P_StoppedHSCP-332_v4/0022/FC351559-2299-DE11-B23A-001A92810AE4.root',
-#    '/store/data/CRAFT09/Calo/RAW-RECO/GR09_31X_V5P_StoppedHSCP-332_v4/0022/F0B42B50-1F99-DE11-B7E4-001A92810A94.root',
-#    '/store/data/CRAFT09/Calo/RAW-RECO/GR09_31X_V5P_StoppedHSCP-332_v4/0022/EAABE6E3-3999-DE11-858E-003048679076.root'
-#    ] )
+readFiles.extend( [
+   '/store/data/CRAFT09/Calo/RAW-RECO/GR09_31X_V5P_StoppedHSCP-332_v4/0022/FC9ECC6F-2299-DE11-BA40-0018F3D09636.root',
+   '/store/data/CRAFT09/Calo/RAW-RECO/GR09_31X_V5P_StoppedHSCP-332_v4/0022/FC6A5AAA-CA98-DE11-A0F2-001A92810AE0.root',
+   '/store/data/CRAFT09/Calo/RAW-RECO/GR09_31X_V5P_StoppedHSCP-332_v4/0022/FC351559-2299-DE11-B23A-001A92810AE4.root',
+   '/store/data/CRAFT09/Calo/RAW-RECO/GR09_31X_V5P_StoppedHSCP-332_v4/0022/F0B42B50-1F99-DE11-B7E4-001A92810A94.root',
+   '/store/data/CRAFT09/Calo/RAW-RECO/GR09_31X_V5P_StoppedHSCP-332_v4/0022/EAABE6E3-3999-DE11-858E-003048679076.root'
+   ] )
 
 # CRAFT 09 Calo PD
-readFiles.extend( [
-       '/store/data/CRAFT09/Calo/RECO/v1/000/111/039/F6DD2D0D-578C-DE11-B40A-001D09F25109.root',
-       '/store/data/CRAFT09/Calo/RECO/v1/000/111/039/F26AF5C0-578C-DE11-82FF-001D09F23C73.root',
-       '/store/data/CRAFT09/Calo/RECO/v1/000/111/039/F07B6B77-538C-DE11-98FA-001617C3B778.root',
-       '/store/data/CRAFT09/Calo/RECO/v1/000/111/039/F05C4FBD-578C-DE11-8114-0019B9F7310E.root',
-       '/store/data/CRAFT09/Calo/RECO/v1/000/111/039/E86BE539-528C-DE11-99BA-001D09F28F1B.root',
-       '/store/data/CRAFT09/Calo/RECO/v1/000/111/039/E2414B50-568C-DE11-8C68-001D09F23E53.root',
-       '/store/data/CRAFT09/Calo/RECO/v1/000/111/039/E04AB303-578C-DE11-B349-003048D37538.root',
-       '/store/data/CRAFT09/Calo/RECO/v1/000/111/039/D69E3B0E-578C-DE11-AB10-003048D2C0F0.root',
-       '/store/data/CRAFT09/Calo/RECO/v1/000/111/039/D66DF737-528C-DE11-8DC5-003048D37456.root',
-       '/store/data/CRAFT09/Calo/RECO/v1/000/111/039/CAA978C7-528C-DE11-9861-001D09F295FB.root',
-       '/store/data/CRAFT09/Calo/RECO/v1/000/111/039/C4EB718F-648C-DE11-8EC9-001D09F2960F.root',
-       '/store/data/CRAFT09/Calo/RECO/v1/000/111/039/C0555F03-578C-DE11-A5A4-003048D2BE12.root',
-       '/store/data/CRAFT09/Calo/RECO/v1/000/111/039/9A5D550D-578C-DE11-9E70-003048D2C0F4.root',
-       '/store/data/CRAFT09/Calo/RECO/v1/000/111/039/92CD0F04-578C-DE11-8C9E-0030486733D8.root',
-       '/store/data/CRAFT09/Calo/RECO/v1/000/111/039/7ECE0404-578C-DE11-AF0F-0030486780B8.root',
-       '/store/data/CRAFT09/Calo/RECO/v1/000/111/039/7C1092C2-528C-DE11-AF45-003048D2BE06.root',
-       '/store/data/CRAFT09/Calo/RECO/v1/000/111/039/7A1C07E2-548C-DE11-88AB-001617E30D40.root',
-       '/store/data/CRAFT09/Calo/RECO/v1/000/111/039/707D6304-578C-DE11-BA85-003048D3750A.root',
-       '/store/data/CRAFT09/Calo/RECO/v1/000/111/039/6EBDD003-578C-DE11-8D4F-003048D2BED6.root',
-       '/store/data/CRAFT09/Calo/RECO/v1/000/111/039/5A1FF5BD-578C-DE11-BD3C-001D09F27003.root',
-       '/store/data/CRAFT09/Calo/RECO/v1/000/111/039/4E77F30A-578C-DE11-B36C-001D09F2910A.root',
-       '/store/data/CRAFT09/Calo/RECO/v1/000/111/039/324AC451-568C-DE11-80AB-001D09F23A02.root',
-       '/store/data/CRAFT09/Calo/RECO/v1/000/111/039/1CDAB003-578C-DE11-BB94-003048D2BE06.root',
-       '/store/data/CRAFT09/Calo/RECO/v1/000/111/039/0818E60D-578C-DE11-8464-001D09F23D04.root',
-       '/store/data/CRAFT09/Calo/RECO/v1/000/111/039/0461CE03-578C-DE11-9ADF-003048D2C020.root',
-       '/store/data/CRAFT09/Calo/RECO/v1/000/111/039/021B1B76-538C-DE11-9868-000423D6B444.root' ] );
+## readFiles.extend( [
+##        '/store/data/CRAFT09/Calo/RECO/v1/000/111/039/F6DD2D0D-578C-DE11-B40A-001D09F25109.root',
+##        '/store/data/CRAFT09/Calo/RECO/v1/000/111/039/F26AF5C0-578C-DE11-82FF-001D09F23C73.root',
+##        '/store/data/CRAFT09/Calo/RECO/v1/000/111/039/F07B6B77-538C-DE11-98FA-001617C3B778.root',
+##        '/store/data/CRAFT09/Calo/RECO/v1/000/111/039/F05C4FBD-578C-DE11-8114-0019B9F7310E.root',
+##        '/store/data/CRAFT09/Calo/RECO/v1/000/111/039/E86BE539-528C-DE11-99BA-001D09F28F1B.root',
+##        '/store/data/CRAFT09/Calo/RECO/v1/000/111/039/E2414B50-568C-DE11-8C68-001D09F23E53.root',
+##        '/store/data/CRAFT09/Calo/RECO/v1/000/111/039/E04AB303-578C-DE11-B349-003048D37538.root',
+##        '/store/data/CRAFT09/Calo/RECO/v1/000/111/039/D69E3B0E-578C-DE11-AB10-003048D2C0F0.root',
+##        '/store/data/CRAFT09/Calo/RECO/v1/000/111/039/D66DF737-528C-DE11-8DC5-003048D37456.root',
+##        '/store/data/CRAFT09/Calo/RECO/v1/000/111/039/CAA978C7-528C-DE11-9861-001D09F295FB.root',
+##        '/store/data/CRAFT09/Calo/RECO/v1/000/111/039/C4EB718F-648C-DE11-8EC9-001D09F2960F.root',
+##        '/store/data/CRAFT09/Calo/RECO/v1/000/111/039/C0555F03-578C-DE11-A5A4-003048D2BE12.root',
+##        '/store/data/CRAFT09/Calo/RECO/v1/000/111/039/9A5D550D-578C-DE11-9E70-003048D2C0F4.root',
+##        '/store/data/CRAFT09/Calo/RECO/v1/000/111/039/92CD0F04-578C-DE11-8C9E-0030486733D8.root',
+##        '/store/data/CRAFT09/Calo/RECO/v1/000/111/039/7ECE0404-578C-DE11-AF0F-0030486780B8.root',
+##        '/store/data/CRAFT09/Calo/RECO/v1/000/111/039/7C1092C2-528C-DE11-AF45-003048D2BE06.root',
+##        '/store/data/CRAFT09/Calo/RECO/v1/000/111/039/7A1C07E2-548C-DE11-88AB-001617E30D40.root',
+##        '/store/data/CRAFT09/Calo/RECO/v1/000/111/039/707D6304-578C-DE11-BA85-003048D3750A.root',
+##        '/store/data/CRAFT09/Calo/RECO/v1/000/111/039/6EBDD003-578C-DE11-8D4F-003048D2BED6.root',
+##        '/store/data/CRAFT09/Calo/RECO/v1/000/111/039/5A1FF5BD-578C-DE11-BD3C-001D09F27003.root',
+##        '/store/data/CRAFT09/Calo/RECO/v1/000/111/039/4E77F30A-578C-DE11-B36C-001D09F2910A.root',
+##        '/store/data/CRAFT09/Calo/RECO/v1/000/111/039/324AC451-568C-DE11-80AB-001D09F23A02.root',
+##        '/store/data/CRAFT09/Calo/RECO/v1/000/111/039/1CDAB003-578C-DE11-BB94-003048D2BE06.root',
+##        '/store/data/CRAFT09/Calo/RECO/v1/000/111/039/0818E60D-578C-DE11-8464-001D09F23D04.root',
+##        '/store/data/CRAFT09/Calo/RECO/v1/000/111/039/0461CE03-578C-DE11-9ADF-003048D2C020.root',
+##        '/store/data/CRAFT09/Calo/RECO/v1/000/111/039/021B1B76-538C-DE11-9868-000423D6B444.root' ] );
 
 
 # CRAFT 08 test RAW data
