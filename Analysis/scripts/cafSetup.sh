f2e06d54537bf2c43f2a453928717185623b6611
jbrooke <>
2010-03-18 13:09:42 +0000

    add caf setup script

---
+++ Analysis/scripts/cafSetup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+pwd=$PWD
+
+# prompt analysis directory
+export CAFDIR=/afs/cern.ch/cms/CAF/CMSCOMM/COMM_TRIGGER/jbrooke/gluinos
+export CMSVER=CMSSW_3_5_2
+
+# GRID UI
+source /afs/cern.ch/cms/LCG/LCG-2/UI/cms_ui_env.sh
+
+# setup CMSSW environment
+cd $CAFDIR/$CMSVER/src
+eval `scram ru -sh`
+cd $pwd
+
+# CRAB
+source /afs/cern.ch/cms/ccs/wm/scripts/Crab/crab.sh
+
