01e577398ce0a5ca6357e995d7c9b2db0de1d7f6
jbrooke <>
2010-03-24 20:40:20 +0000

    use gpfs

--- Analysis/scripts/mergeFiles.sh
+++ Analysis/scripts/mergeFiles.sh
@@ -1,25 +1,31 @@
 #!/bin/bash
 
 label=$1
-nfiles=$2
-first=$3
 
-remote=srm://lcgse02.phy.bris.ac.uk:8444/cms/store/user/jbrooke/
+source=/gpfs_phys/storm/cms/user/jbrooke/
 local=/storage/phjjb/stoppedHSCP
 file=stoppedHSCPTree
-enddir=~/stoppedHSCP/ntuples/
+dest=~/stoppedHSCP/ntuples/
 
-# copy files to local storage
+# create local storage
 if [ ! -e $local/$label ]
     then mkdir $local/$label
 fi
-echo "copy.py $remote/StoppedHSCP_$label $local/$label $file $nfiles $first"
-copy.py $remote/StoppedHSCP_$label $local/$label $file $nfiles $first
 
-# merge them with hadd
-echo "merge.py $local/$label $file $nfiles $first"
-merge.py $local/$label $file $nfiles $first
+# copy files
+echo "cp $source/$label/stoppedHSCPTree*.root $local/$label/."
+cp $source/$label/stoppedHSCPTree*.root $local/$label/.
+
+# merge them with hadd (remove output first to avoid duplicating events!)
+if [ -e $local/$label/$label.root ]
+    then rm $local/$label/$label.root
+fi
+
+ls -l $local/$label
+
+echo "hadd $local/$label/$label.root $local/$label/*.root"
+hadd $local/$label/$label.root $local/$label/*.root
 
 # copy merged file to ntuples dir
-echo "cp $local/$label/$file.root ~/stoppedHSCP/ntuples/$file\_$label.root"
-cp $local/$label/$file.root ~/stoppedHSCP/ntuples/$file\_$label.root
+echo "cp $local/$label/$label.root $dest/$label.root"
+cp $local/$label/$label.root $dest/$label.root
