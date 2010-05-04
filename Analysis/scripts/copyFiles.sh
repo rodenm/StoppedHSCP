ae61075e27212ed95d62f19c606bbd8dde816b92
jbrooke <>
2010-05-04 14:00:44 +0000

    new version

---
+++ Analysis/scripts/copyFiles.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# copy files from grid location to local disk
+
+label=$1
+nfiles=$2
+first=$3
+crab=$4
+
+griddir=srm://heplnx204.pp.rl.ac.uk:8443/srm/managerv2?SFN=/pnfs/pp.rl.ac.uk/data/cms/store/user/jbrooke
+odir=/storage/phjjb/stoppedHSCP
+filebase=stoppedHSCPTree
+
+echo "Copying files from $griddir to $odir"
+
+for i in $(seq $first 1 $nfiles)
+do
+  echo "lcg-cp $griddir/$label/${filebase}_${i}_${crab}.root $odir/$label/${filebase}_$i.root"
+  lcg-cp $griddir/$label/$filebase\_$i\_$crab.root $odir/$label/$filebase\_$i.root
+done
