2c5613c883ff46caa904feb5dab1aa171af2bd39
jbrooke <>
2010-07-05 10:34:30 +0000

    first commit

---
+++ ToyMC/src/LinkDef.h
@@ -0,0 +1,10 @@
+#include "StoppedHSCP/ToyMC/interface/Experiment.h"
+
+#ifdef __CINT__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+#pragma link C++ class Experiment;
+#endif
