1dcbfab0316c92b16b7cf89e8becbf2e61f52fad
jbrooke <>
2010-07-30 11:07:58 +0000

    cleanup

--- Skimming/python/rHadronCombinedPath_cff.py
+++
@@ -1,5 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-from SUSYBSMAnalysis.RHadron.rHadronHLTPath_cfi import rHadronHLTPath
-from SUSYBSMAnalysis.RHadron.rHadronFilter_cfi  import rHadronFilter
-
-rHadronPath = cms.Path( rHadronHLTPath + rHadronFilter )
--- Skimming/python/rHadronHLTPath_cfi.py
+++
@@ -1,6 +0,0 @@
-
-import FWCore.ParameterSet.Config as cms
-
-from HLTrigger.HLTfilters.hltHighLevel_cfi import hltHighLevel as rHadronHLTPath
-
-rHadronHLTPath.HLTPaths = cms.vstring( "HLTStoppedHSCPPath" )
