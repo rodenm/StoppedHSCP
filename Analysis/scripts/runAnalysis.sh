388e217c8b5b4d74a553539d4e396d4327db3346
jbrooke <>
2010-08-24 15:13:31 +0000

    cleanup obsolete files

---
+++ Analysis/scripts/runAnalysis.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# run all steps of the analysis
+
+
+dataset = $1
+runs = $2
+
+# make histograms
+makeHistograms ntuples/stoppedHSCP_tree_$dataset $runs
+
+# make plots
+python $CMSSW_BASE/src/StoppedHSCP/Analysis/macros/makePlots.py $dataset
+python $CMSSW_BASE/src/StoppedHSCP/Analysis/macros/makePlotsByRun.py $dataset $runs
+
+# print summary
+python $CMSSW_BASE/src/StoppedHSCP/Analysis/macros/printSummary.py $dataset
+
+# make Toy MC jobs
+makeToyJobs.py $dataset
