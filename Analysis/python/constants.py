8dc021415168a71a54935674ba19979b59fb3640
jbrooke <>
2010-10-12 19:57:30 +0000

    add bg estimate code

--- Analysis/python/constants.py
+++ Analysis/python/constants.py
@@ -3,16 +3,18 @@
 lumiBlockLength = 25.e-9 * 3564. * pow(2., 18)
 
 # cuts
-nCuts    = 15
-iJet50   = 7
-in90     = 9
-iCT      = 10
-iAllCuts = 14
+nCuts    = 13
+iJet50   = 5
+in90     = 7
+iCT      = 8
+iAllCuts = 12
+
+# BG rate estimate (naive)
+bg     = 5.6e-5
+ebg    = 1.3e-5
 
 # BG rate estimate coeffs
-fall     = 5.6e-5
-efall    = 1.3e-5
-fn90     = 0.029
+fn90     = 2.861e-02 
 efn90    = fn90 * 0.26
-fct      = 0.041
+fct      = 4.060e-02
 efct     = fct * 0.26
--- Analysis/python/printSummary.py
+++ Analysis/python/printSummary.py
@@ -91,54 +91,63 @@ if isMC:
 else:
     print "|Cut\t|N\t|Rate (Hz) |ind %\t|cum %\t|N-1 % |N-1 (Hz)|-"
 
-for i in range(0,15):
+for i in range(0,nCuts):
     ncum = hcutcum.GetBinContent(i+1)
     nind = hcutind.GetBinContent(i+1)
     nnmo = hcutnmo.GetBinContent(i+1)
     label = hcutcum.GetXaxis().GetBinLabel(i+1)
     if isMC:
-        print '|%s | %i | %.2e | %.2e |' % (label, ncum, 100.*ncum/ntot, 100.*nnmo/ntot)
+        print '|%i %s | %i | %.2e | %.2e |' % (i, label, ncum, 100.*ncum/ntot, 100.*nnmo/ntot)
     else:
-        print '|%s | %i | %.2e | %.2e | %.2e | %.2e | %.2e |-' % (label, ncum, ncum/time, 100.*nind/ntot, 100.*ncum/ntot, 100.*nnmo/ntot, nnmo/time)
+        print '|%i %s | %i | %.2e | %.2e | %.2e | %.2e | %.2e |-' % (i, label, ncum, ncum/time, 100.*nind/ntot, 100.*ncum/ntot, 100.*nnmo/ntot, nnmo/time)
 
 print '[/TABLE]'
 print
 
 # backgrounds
-n90rate = hcutnmo.GetBinContent(in90+1)/time
-ctrate  = hcutnmo.GetBinContent(iCT+1)/time
-
-# naive method
-bg0  = fall
-ebg0 = efall
+n90nmo   = int(hcutnmo.GetBinContent(in90+1))
+ctnmo    = int(hcutnmo.GetBinContent(iCT+1))
+fincount = hcutcum.GetBinContent(iAllCuts+1)
 
 # n90 method
-bg1  = fn90*n90rate
-ebg1 = efn90*n90rate
+bg1  = fn90*n90nmo/time
+ebg1 = efn90*n90nmo/time
 
 # CT method
-bg2  = fct*ctrate
-ebg2 = efct*ctrate
+bg2  = fct*ctnmo/time
+ebg2 = efct*ctnmo/time
 
 # combined n90 & CT
 bg3  = (bg1 + bg2) / 2
 ebg3 = 0.
 
-print "N-1 counts :"
-print '  n60    : ' % (hcutnmo.GetBinContent(in90+1))
-print '  CT     : ' % (hcutnmo.GetBinContent(iCT+1))
+print "N-1 rates"
+print '  n90   : %.2e ' % (n90nmo/time)
+print '  CT    : %.2e ' % (ctnmo/time)
 print
 
-print "Expected background rates :"
-print '  Simple method : %s +/- %s' % (bg0, ebg0)
-print '  N90 method    : %s +/- %s' % (bg1, ebg1)
-print '  CT method     : %s +/- %s' % (bg2, ebg2)
-print '  N90 & CT      : %s +/- %s' % (bg3, ebg3)
+print "N-1 rate coefficients"
+print '  n90   : %.3e ' % (fincount/n90nmo)
+print '  ct    : %.3e ' % (fincount/ctnmo)
 print
 
-print "Expected background counts :"
-print '  Simple method : %s +/- %s' % (bg0*time, ebg0*time)
-print '  N90 method    : %s +/- %s' % (bg1*time, ebg1*time)
-print '  CT method     : %s +/- %s' % (bg2*time, ebg2*time)
-print '  N90 & CT      : %s +/- %s' % (bg3*time, ebg3*time)
+print "Expected background rates"
+print '  Simple method : %.2e +/- %.2e' % (bg, ebg)
+print '  N90 method    : %.2e +/- %.2e' % (bg1, ebg1)
+print '  CT method     : %.2e +/- %.2e' % (bg2, ebg2)
+print '  N90 & CT      : %.2e +/- %.2e' % (bg3, ebg3)
+print
+
+print "Expected background counts"
+print '  Simple method : %.2e +/- %.2e' % (bg*time, ebg*time)
+print '  N90 method    : %.2e +/- %.2e' % (bg1*time, ebg1*time)
+print '  CT method     : %.2e +/- %.2e' % (bg2*time, ebg2*time)
+print '  N90 & CT      : %.2e +/- %.2e' % (bg3*time, ebg3*time)
+print
+
+print "Final count : %i" % (fincount)
+print
+
+print "Final rate : %.2e +/- %.2e" % (fincount/time, sqrt(fincount)/time)
+print
 
