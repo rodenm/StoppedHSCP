1be2a3018934291071d531c2123a3cb736ea61d8
jbrooke <>
2011-03-10 11:51:28 +0000

    remove old idea

--- Analysis/src/LinkDef.h
+++ Analysis/src/LinkDef.h
@@ -1,7 +1,6 @@
 #include <vector>
 #include "boost/cstdint.hpp"
 #include "StoppedHSCP/Analysis/interface/StoppedHSCPEvent.h"
-#include "StoppedHSCP/Analysis/interface/StoppedHSCPRun.h"
 
 #ifdef __CINT__
 
@@ -11,5 +10,4 @@
 
 #pragma link C++ class std::vector<ULong64_t>;
 #pragma link C++ class StoppedHSCPEvent;
-#pragma link C++ class StoppedHSCPRun;
 #endif
