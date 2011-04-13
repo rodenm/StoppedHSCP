30aa43d7114a8d97155a5c351d1ea0950c54f41c
jbrooke <>
2011-04-13 13:54:44 +0000

    first commit

---
+++ Analysis/interface/Constants.h
@@ -0,0 +1,8 @@
+
+
+static const unsigned NBX_PER_ORBIT = 3564;
+static const double TIME_PER_BX     = 25.e-9;
+
+static const double TIME_PER_LS     = 23.3570304;
+
+static const double TIME_WINDOW     = 1.256;
