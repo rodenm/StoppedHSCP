965456addcea93ac041815fb4d25243f5b941ff4
jbrooke <>
2011-04-27 10:33:18 +0000

    fix 2010 HLT path wildcard

--- Analysis/python/StoppedHSCP_Trigger2010_cff.py
+++ Analysis/python/StoppedHSCP_Trigger2010_cff.py
@@ -3,9 +3,9 @@
 
 import FWCore.ParameterSet.Config as cms
 
-HLTPaths = cms.vstring("HLT_StoppedHSCP_*")
+HLTPaths = cms.vstring("HLT_StoppedHSCP*")
 
-hltPathJetNoBptx = cms.untracked.string("HLT_StoppedHSCP_*")
+hltPathJetNoBptx = cms.untracked.string("HLT_StoppedHSCP*")
 hltPathJetNoBptxNoHalo = cms.untracked.string("")
 hltPathJetNoBptx3BXNoHalo = cms.untracked.string("")
 hltL3Tag = cms.untracked.InputTag("hltStoppedHSCP1CaloJetEnergy30","","HLT")
