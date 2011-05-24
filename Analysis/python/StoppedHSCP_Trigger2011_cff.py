dc8f4fdecbaecbaaf8536a07d360eb8dba033857
temple <>
2011-05-24 18:57:36 +0000

    removed wildcards from trigger names

--- Analysis/python/StoppedHSCP_Trigger2011_cff.py
+++ Analysis/python/StoppedHSCP_Trigger2011_cff.py
@@ -2,14 +2,15 @@
 # override trigger settings for 2011 triggers
 
 import FWCore.ParameterSet.Config as cms
-
+#Wild cards are okay for these names
 HLTPaths = cms.vstring(
     "HLT_JetE30_NoBPTX_v*",
     "HLT_JetE30_NoBPTX_NoHalo_v*",
     "HLT_JetE30_NoBPTX3BX_NoHalo_v*"
 )
 
-hltPathJetNoBptx = cms.untracked.string("HLT_JetE30_NoBPTX_v*")
-hltPathJetNoBptxNoHalo = cms.untracked.string("HLT_JetE30_NoBPTX_NoHalo_v*")
-hltPathJetNoBptx3BXNoHalo = cms.untracked.string("HLT_JetE30_NoBPTX3BX_NoHalo_v*")
+# Wild cards are not currently supported for these strings
+hltPathJetNoBptx = cms.untracked.string("HLT_JetE30_NoBPTX_v")
+hltPathJetNoBptxNoHalo = cms.untracked.string("HLT_JetE30_NoBPTX_NoHalo_v")
+hltPathJetNoBptx3BXNoHalo = cms.untracked.string("HLT_JetE30_NoBPTX3BX_NoHalo_v")
 hltL3Tag = cms.untracked.InputTag("hltStoppedHSCPTight1CaloJetEnergy30","","HLT")
