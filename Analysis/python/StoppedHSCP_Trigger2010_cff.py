67cc0257d8e3821f7d1304f6c961e3822a6ff44e
jbrooke <>
2011-06-01 15:46:40 +0000

    fix HLT L3 filter

--- Analysis/python/StoppedHSCP_Trigger2010_cff.py
+++ Analysis/python/StoppedHSCP_Trigger2010_cff.py
@@ -8,5 +8,7 @@ HLTPaths = cms.vstring("HLT_StoppedHSCP*")
 hltPathJetNoBptx = cms.untracked.string("HLT_StoppedHSCP")
 hltPathJetNoBptxNoHalo = cms.untracked.string("")
 hltPathJetNoBptx3BXNoHalo = cms.untracked.string("")
-hltL3Tag = cms.untracked.InputTag("hltStoppedHSCP1CaloJetEnergy30","","HLT")
+
+# this works for Run2010A, but will break during Run2010B
+hltL3Tag = cms.untracked.InputTag("hltStoppedHSCP1CaloJetEnergy","","HLT") 
 
