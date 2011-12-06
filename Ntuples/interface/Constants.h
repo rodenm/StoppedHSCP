0a1aed7edda29b0b2a4062e9215ad6d6f714065c
jbrooke <>
2011-12-06 14:50:26 +0000

    change to correct LHC clock

--- Ntuples/interface/Constants.h
+++ Ntuples/interface/Constants.h
@@ -1,10 +1,8 @@
-#ifndef GUARD_Constants_h
-#define GUARD_Constants_h
-
-//namespace shscp {
+#ifndef Constants_h
+#define Constants_h
 
 const unsigned NBX_PER_ORBIT = 3564;
-const double TIME_PER_BX     = 25.e-9;
+const double TIME_PER_BX     = 24.95097e-9;
 const unsigned NORBIT_PER_LS = 262144;
 
 const double TIME_PER_LS     = TIME_PER_BX * NBX_PER_ORBIT * NORBIT_PER_LS;
@@ -14,8 +12,5 @@ const double TIME_WINDOW     = 1.256;
 
 const int BX_VETO_MINUS = -2;
 const int BX_VETO_PLUS = 2;
-
-
-//}
 
 #endif
