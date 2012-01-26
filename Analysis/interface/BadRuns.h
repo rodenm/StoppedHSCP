a42e3fc77875d972796c977333ad278183559a88
temple <>
2012-01-26 21:08:19 +0000

    Added BadRuns header

---
+++ Analysis/interface/BadRuns.h
@@ -0,0 +1,13 @@
+#ifndef GUARD_badruns_h
+#define GUARD_badruns_h
+
+
+// explicitly checks for bad runs and fill numbers
+bool isBadFillOrRun(int fill, int run);
+
+// checks for bad runs (including those from bad fills, but 
+// those runs are hard coded based on the info in fills.txt from
+// 16 January 2012)
+bool isBadRun(unsigned long run);
+
+#endif
