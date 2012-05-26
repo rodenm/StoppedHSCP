a17c0cfc3afe348e93f5e047049cf4cd1f0c74ca
rodenm <>
2012-05-26 01:28:33 +0000

    Added /NoBPTX/Run2012B-PromptReco-v1/RECO

--- Ntuples/scripts/SetDatasetInfo.py
+++ Ntuples/scripts/SetDatasetInfo.py
@@ -148,7 +148,16 @@ def GetDatasets():
     datasets[name].InputInfo["l1JetNoBptxNoHaloName"]='L1_SingleJetC20_NotBptxOR'
     datasets[name].InputInfo["l1Jet32NoBptxNoHaloName"]='L1_SingleJetC32_NotBptxOR'
                      
-
+    #/NoBPTX/Run2012B-PromptReco-v1/RECO
+    name="/NoBPTX/Run2012B-PromptReco-v1/RECO"
+    datasets[name]=DatasetInfo(name=name,
+                               trigger="2012",
+                               datatype="RECO")
+    datasets[name].InputInfo["hltL3Tag"]="hltStoppedHSCP1CaloJetEnergy70"
+    datasets[name].InputInfo["l1JetNoBptxName"]='L1_SingleJetC20_NotBptxOR'
+    datasets[name].InputInfo["l1JetNoBptxNoHaloName"]='L1_SingleJetC20_NotBptxOR'
+    datasets[name].InputInfo["l1Jet32NoBptxNoHaloName"]='L1_SingleJetC32_NotBptxOR'
+                           
     return datasets
 
 
