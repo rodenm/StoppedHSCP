269960947ffb602e4abf9da4c26738d68fa9dfdb
rodenm <>
2013-07-16 17:57:23 +0000

    include fancy plots from 2011 in makeFinalPlots.sh

---
+++ Analysis/macros/allInOneLifetime.C
@@ -0,0 +1,248 @@
+#include <sstream>
+
+#include "TGraph.h"
+#include "TGaxis.h"
+#include "TF1.h"
+#include "TGraphAsymmErrors.h"
+#include "TCanvas.h"
+#include "TFile.h"
+#include "TH1.h"
+#include "TH1F.h"
+#include "TLegend.h"
+#include "TLatex.h"
+#include "TPaveText.h"
+
+#include "ExtraLimitPlots.h"
+
+struct ExtraAxis {
+  vector<TObject*> objects;
+  void Add (TObject* object) {objects.push_back(object);}
+  void Draw () {
+    for (size_t i = 0; i < objects.size(); ++i) objects[i]->Draw(); 
+  }
+};
+
+ExtraAxis anotherScale (const TH1* refHist, double scale, int color, const char* title, double offset) {
+  ExtraAxis result;
+  double x0 = refHist->GetXaxis()->GetXmin();
+  double x1 = refHist->GetXaxis()->GetXmax();
+  double y0 = refHist->GetMinimum();
+  double y1 = refHist->GetMaximum();
+  // double y0 = refHist->GetYaxis()->GetXmin();
+  // double y1 = refHist->GetYaxis()->GetXmax();
+  double xoffset = exp (log(x0) - (log(x1) - log(x0))*offset);
+
+  TGaxis* axis = new TGaxis(xoffset, y0, xoffset, y1, y0*scale,y1*scale,510,"-GS");
+  axis->ImportAxisAttributes (refHist->GetXaxis());
+  axis->SetTitle(title);
+  axis->SetTextColor (color);
+  axis->SetLineColor (color);
+  axis->SetLineWidth (1);
+  axis->SetTextColor (color);
+  axis->SetLabelColor (color);
+  axis->SetLabelOffset (0.);
+  axis->SetTitleOffset (0.65);
+  axis->SetTickSize(0.015);	
+  result.Add (axis);
+
+  TLine* line = new TLine (xoffset, y0, xoffset, y1);
+  line->SetLineColor (color);
+  line->SetLineWidth (2);
+  result.Add (line);
+
+  line = new TLine (x0, y0, xoffset, y0);
+  line->SetLineColor (kGray);
+  line->SetLineWidth (2);
+  result.Add (line);
+  
+  line = new TLine (x0, y1, xoffset, y1);
+  line->SetLineColor (kGray);
+  line->SetLineWidth (2);
+  result.Add (line);
+
+  return result;
+}
+
+void allInOneLifetime(double lumi=4560., double maxInstLumi=5000.) {
+
+  ExtraLimitPlots plots(lumi);
+  plots.calculateCrossSections(12,3,3,39,9);
+  
+  // graphs - observed
+  TGraph* g_obs      = plots.getObsLimit();
+  TGraph* g_exp      = plots.getExpLimit();
+  TGraphAsymmErrors* g_exp_1sig = plots.getExpLimit1Sig();
+  TGraphAsymmErrors* g_exp_2sig = plots.getExpLimit2Sig();
+
+  TGraph* g_obs_gluino = plots.getLimitGluino();
+  double gluino2ref    = g_obs_gluino->GetY()[0] / g_obs->GetY()[0];
+  TGraph* g_obs_stop   = plots.getLimitStop();
+  double stop2ref      = g_obs_stop->GetY()[0] / g_obs->GetY()[0];
+  TGraph* g_obs_stau   = plots.getLimitStau();
+  double stau2ref      = g_obs_stau->GetY()[0] / g_obs->GetY()[0];
+
+  cout << "scales: " << g_obs->GetY()[0] 
+       << '/' <<g_obs_gluino->GetY()[0]
+       << '/' <<g_obs_stop->GetY()[0]
+       << '/' <<g_obs_stau->GetY()[0]
+       <<endl;
+
+  
+  TCanvas *canvas = new TCanvas("allLifetime", "allLifetime", 1000, 600);
+  
+  canvas->SetLogx();
+  canvas->SetLogy();
+
+  canvas->SetRightMargin(0.8*canvas->GetLeftMargin());
+  canvas->SetLeftMargin(1.2*canvas->GetLeftMargin());
+  canvas->SetTicks (canvas->GetTickx(), 0);
+  
+  TH1F* h = new TH1F ("h", "", 1, 7.5e-8, 1e6);
+  h->SetStats (0);
+  h->SetMinimum (.0001);
+  h->SetMaximum (0.99e1);
+  // TH1* h = canvas->DrawFrame(7.5e-8, .001, 1e6, 1e2, "Y+");
+  h->SetTitle("Beamgap Expt");
+  //  h->GetXaxis()->SetTitle("#tau_{#tilde{g},#tilde{t},#tilde{#tau}} [s]");
+  h->GetXaxis()->SetTitle("#tau [s]");
+  h->GetYaxis()->SetTitle("#sigma #times BF #times #varepsilon_{stop} #times #varepsilon_{det}   [pb]  ");
+  h->Draw ("Y+");
+
+  ExtraAxis aGluino = anotherScale (h, gluino2ref, kRed+2, "#sigma(pp #rightarrow #tilde{g}#tilde{g}) #times BF(#tilde{g} #rightarrow g#tilde{#chi}^{0})   [pb]  ", 0.0);
+
+  ExtraAxis aStop = anotherScale (h, stop2ref, kBlue+2, "#sigma(pp #rightarrow #tilde{t}#tilde{t}) #times BF(#tilde{t} #rightarrow t#tilde{#chi}^{0})   [pb]  ", 0.2);
+
+  ExtraAxis aStau = anotherScale (h, stau2ref, kGreen+2, "#sigma(pp #rightarrow #tilde{#tau}#tilde{#tau}) #times BF(#tilde{#tau} #rightarrow #tau#tilde{#chi}^{0})   [pb]  ", 0.4);
+
+
+  
+  
+  TPaveText* blurb = new TPaveText(0.25, 0.57, 0.50, 0.87, "NDC");
+
+  blurb->AddText("CMS Preliminary 2012");
+  // std::stringstream label;
+  // label<<"#int L dt = "<<lumi<<" pb^{-1}";
+  // blurb->AddText(label.str().c_str());
+  // double peakInstLumi=maxInstLumi;
+  // int exponent=30;
+  // while (peakInstLumi>10) {
+  //   peakInstLumi/=10;
+  //   ++exponent;
+  // }
+  // std::stringstream label2;
+  // label2<<"L^{max}_{inst} = "<<peakInstLumi<<" x 10^{"<<exponent<<"} cm^{-2}s^{-1}";
+  // blurb->AddText(label2.str().c_str());
+
+  //blurb->AddText("CMS 2011");
+  blurb->AddText("#int L dt = 18.6 fb^{-1}");//,  #int L_{eff} dt = 935 pb^{-1}");
+  //blurb->AddText("L^{max}_{inst} = 3.5 #times 10^{33} cm^{-2}s^{-1}");
+  blurb->AddText("#sqrt{s} = 8 TeV");
+  blurb->AddText("E_{gluon} > 100 GeV, E_{top} > 125 GeV");
+  //blurb->AddText("m_{#tilde{g}} = 300 GeV/c^{2}");
+  //blurb->AddText("m_{#tilde{#chi}^{0}} = 200 GeV/c^{2}");
+  blurb->SetTextFont(42);
+  blurb->SetBorderSize(0);
+  blurb->SetFillColor(0);
+  blurb->SetShadowColor(0);
+  blurb->SetTextAlign(12);
+  blurb->SetTextSize(0.033);
+  blurb->Draw();
+  
+  
+  // 2 sigma band
+  if (g_exp_2sig) {
+    g_exp_2sig->SetLineColor(0);
+    g_exp_2sig->SetLineStyle(0);
+    g_exp_2sig->SetLineWidth(0);
+    g_exp_2sig->SetFillColor(kYellow);
+    g_exp_2sig->SetFillStyle(1001);
+    g_exp_2sig->Draw("3");
+  }
+  
+  // 1 sigma band
+  if (g_exp_1sig) {
+    // g_exp_1sig->SetLineColor(8);
+    g_exp_1sig->SetLineColor(0);
+    g_exp_1sig->SetLineStyle(0);
+    g_exp_1sig->SetLineWidth(0);
+    // g_exp_1sig->SetFillColor(8);
+    g_exp_1sig->SetFillColor(kGreen);
+    g_exp_1sig->SetFillStyle(1001);
+    // g_exp_1sig->SetFillStyle(3005);
+    g_exp_1sig->Draw("3");
+    // g_exp_1sig->Draw("lX");
+  }
+  
+  
+  // GLUINO LIMIT
+  if (g_exp) {
+    g_exp->SetLineColor(1);
+    g_exp->SetLineStyle(4);
+    g_exp->SetLineWidth(2);
+    g_exp->Draw("l3");
+  }
+  
+  TLine *l;
+  l = new TLine(7.5e-8, 1.3/gluino2ref, 1e6, 1.3/gluino2ref); //600 GeV
+  l->SetLineColor(kRed);
+  l->SetLineWidth(2);
+  l->Draw();
+  
+  TLatex *t1;
+  t1 = new TLatex(0.1, 0.6/gluino2ref, "#sigma_{theory} (m_{#tilde{g}} = 600 GeV)");
+  t1->SetTextColor(kRed);
+  t1->SetTextFont(42);
+  t1->SetTextSize(0.035);
+  t1->Draw();
+
+  // STOP LIMIT
+  TLine *ltop = new TLine(7.5e-8, 0.025/stop2ref, 1e6, 0.025/stop2ref); //600 GeV
+  ltop->SetLineColor(kBlue);
+  ltop->SetLineWidth(2);
+  ltop->Draw();
+  
+  TLatex *t1top;
+  t1top = new TLatex(0.1, 0.01/stop2ref, "#sigma_{theory} (m_{#tilde{t}} = 600 GeV)");
+  t1top->SetTextColor(kBlue);
+  t1top->SetTextFont(42);
+  t1top->SetTextSize(0.035);
+  t1top->Draw();
+  
+
+  // observed limit
+  if (g_obs) {
+    g_obs->SetLineColor(1);
+    g_obs->SetLineStyle(1);
+    g_obs->SetLineWidth(2);
+    g_obs->Draw("l");
+  }
+  
+  
+
+  TLegend* leg = new TLegend(0.67, 0.65, 0.82, 0.87,"95% CL Limits:","NDC");
+  leg->SetTextSize(0.033);
+  leg->SetBorderSize(0);
+  leg->SetTextFont(42);
+  leg->SetFillColor(0);
+  TGraph* expectedStyle1 = new TGraph (*g_exp);
+  expectedStyle1->SetFillColor (g_exp_1sig->GetFillColor());
+  TGraph* expectedStyle2 = new TGraph (*g_exp);
+  expectedStyle2->SetFillColor (g_exp_2sig->GetFillColor());
+  cout << "colors: " << g_exp_1sig->GetFillColor() << ':' << g_exp_2sig->GetFillColor() << endl;
+  leg->AddEntry(g_obs, "Observed", "l");
+  leg->AddEntry(expectedStyle1, "Expected #pm1#sigma", "lf");
+  leg->AddEntry(expectedStyle2, "Expected #pm2#sigma", "lf");
+  //leg->AddEntry(g_obs_stop,"Obs.: Counting Exp. (#tilde{t})", "l");
+  //leg->AddEntry(g_obs_nb, "Obs.: Counting Exp. (Neutral R-Baryon)", "l");
+  //leg->AddEntry(g_obs_em, "Observed: Counting Exp. (EM only)", "l");
+  //leg->AddEntry(g_obs_tp, "Observed: Timing Profile", "l");
+  leg->Draw();
+
+  h->Draw("sameaxis y+");
+  aGluino.Draw();
+  aStop.Draw();
+  //aStau.Draw();
+
+  canvas->Print("allInOneLifetime.png");
+  canvas->Print("allInOneLifetime.pdf");
+}
---
+++ Analysis/macros/gluinostopMassLifetime.C
@@ -0,0 +1,340 @@
+#include <sstream>
+
+#include "TGraph.h"
+#include "TGaxis.h"
+#include "TF1.h"
+#include "TGraphAsymmErrors.h"
+#include "TCanvas.h"
+#include "TFile.h"
+#include "TH1.h"
+#include "TH1F.h"
+#include "TLegend.h"
+#include "TLatex.h"
+#include "TPaveText.h"
+#include "TArrow.h"
+
+#include "ExtraLimitPlots.h"
+
+class Xsection {
+  vector<double> masses;
+  vector<double> logXsecs;
+public:
+  Xsection (const vector<double>& m, const double* xs) {
+    masses = m;
+    for (size_t i = 0; i < masses.size(); ++i) logXsecs.push_back (xs[i]);
+  }
+  double mass2xsec (double mass) {
+    double logXsec = 0;
+    if (mass < masses.front() || mass > masses.back()) {
+      cerr << "gluinoXsection::mass2xsec-> mass is beyond the range: " << mass << endl;
+      return 0;
+    }
+    int index = 0;
+    while (mass > masses[index]) ++index;
+    if (index == 0) logXsec = logXsecs[0];
+    else logXsec = logXsecs[index-1] + (mass-masses[index-1])*(logXsecs[index]-logXsecs[index-1])/(masses[index]-masses[index-1]);
+    return 1.e-3*pow(10, logXsec/20.);
+  }
+  double xsec2mass (double xsec) {
+    double logXsec = log10(xsec/1e-3)*20.;
+    if (logXsec > logXsecs.front() || logXsec < logXsecs.back()) {
+      cerr << "gluinoXsection::xsec2mass-> xsec is beyond the range: " 
+	   << xsec << '/' << logXsec << '/' <<logXsecs.front() << '/' << logXsecs.back() << endl;
+      return 0;
+    }
+    double mass = 0;
+    int index = 0;
+    while (logXsec < logXsecs[index]) ++index;
+    if (index == 0) mass = masses[0];
+    else mass = masses[index-1] + (logXsec-logXsecs[index-1])*(masses[index]-masses[index-1])/(logXsecs[index]-logXsecs[index-1]);
+    // cout << "xsec2mass -> "<<xsec<< " ("<<logXsec<<") -> " << mass << endl;
+    return mass;
+  }
+
+  void xsec2mass (TGraph* g) {
+    for (int i = 0; i < g->GetN(); ++i) {
+      g->GetY()[i] = xsec2mass (g->GetY()[i]);
+    }
+  }
+
+  void xsec2mass (TGraphAsymmErrors* g) {
+    for (int i = 0; i < g->GetN(); ++i) {
+      double xs = g->GetY()[i];
+      double xsPlus = g->GetEYhigh()[i];
+      double xsMius = g->GetEYlow()[i];
+      g->GetY()[i] = xsec2mass (xs);
+      g->GetEYhigh()[i] = xsec2mass (xs-xsMius) -  g->GetY()[i];
+      g->GetEYlow()[i] = g->GetY()[i] - xsec2mass (xs+xsPlus);
+    }
+  }
+};
+
+void gluinostopMassLifetime(double lumi=4560., double maxInstLumi=5000.) {
+
+  ExtraLimitPlots plots(lumi);
+  plots.calculateCrossSections(12,3,3,39,9);
+
+  // xsecs as extracted from nllfast http://web.physik.rwth-aachen.de/service/wiki/bin/view/Main/SquarksandGluinos
+  // gluino (m_squark=m_gluino):  "nllfast gg mstw <mass> <mass>"
+  // gluino (m_squark>>m_gluino): "nllfast gdcpl mstw <mass>"
+  // stop: "nllfast st mstw <mass>"
+  double g_mass [21] = {  
+    200 ,     250 ,     300 ,     350 ,     400 ,   
+    450 ,     500 ,     550 ,     600 ,     650 ,   
+    700 ,     750 ,     800 ,     850 ,     900 ,   
+    950 ,    1000 ,    1050 ,    1100 ,    1150 ,   
+    1200 };
+  double g_xsec [21] = {  
+    606 ,     168 ,    57.2 ,    21.8 ,    8.98 ,   
+    4 ,    1.87 ,   0.917 ,   0.465 ,   0.243 ,   
+    0.13 ,  0.0712 ,  0.0396 ,  0.0224 ,  0.0128 ,   
+    0.00739 ,  0.0043 ,  0.00252 ,  0.00149 ,  0.000883 ,   
+    0.000525 };
+  double g_xsecdcpl [21] = {  
+    706 ,     203 ,    69.7 ,    27.1 ,    11.6 ,   
+    5.29 ,    2.56 ,    1.29 ,   0.679 ,   0.368 ,   
+    0.203 ,   0.115 ,  0.0663 ,  0.0387 ,  0.0229 ,   
+    0.0137 ,  0.00826 ,  0.00502 ,  0.00306 ,  0.00188 ,   
+    0.00115 };
+  double s_mass [21] = {  
+    100 ,     150 ,     200 ,     250 ,     300 ,   
+    350 ,     400 ,     450 ,     500 ,     550 ,   
+    600 ,     650 ,     700 ,     750 ,     800 ,   
+    850 ,     900 ,     950 ,    1000 ,    1050 ,   
+    1100 };
+  double s_xsec [21] = {  
+    424 ,    58.5 ,      13 ,    3.79 ,    1.31 ,   
+    0.515 ,    0.22 ,   0.101 ,  0.0485 ,  0.0244 ,   
+    0.0127 ,  0.00679 ,  0.00372 ,  0.00207 ,  0.00118 ,   
+    0.000678 ,  0.000394 ,  0.000231 ,  0.000137 ,       0 ,   
+    0 };
+
+  //gluino xsec
+  vector<double> masses;
+  for (int i = 0; i < 21; ++i)  {
+    masses.push_back(g_mass[i]);
+    g_xsec [i] = log10 (g_xsec [i]*1e3) * 20.;
+    g_xsecdcpl [i] = log10 (g_xsecdcpl [i]*1e3) * 20.;
+  }
+  //  Xsection gluino_xs (masses, g_xsec);
+  Xsection gluino_xs (masses, g_xsecdcpl);
+  
+  //stop xsec
+  masses.clear();
+  for (int i = 0; i < 19; ++i)  {
+    masses.push_back(s_mass[i]);
+    s_xsec [i] = log10 (s_xsec [i]*1e3) * 20.;
+  }
+  Xsection stop_xs (masses, s_xsec);
+  
+  // graphs
+  TGraph* g_obs     = new TGraph (*plots.getLimitGluino());
+  gluino_xs.xsec2mass (g_obs);
+
+  TGraph* g_exp      = new TGraph (*plots.getExpLimitGluino());
+  gluino_xs.xsec2mass (g_exp);
+  TGraphAsymmErrors* g_exp_1sig = new TGraphAsymmErrors (*plots.getExpLimitGluino1Sig());
+  gluino_xs.xsec2mass (g_exp_1sig);
+  TGraphAsymmErrors* g_exp_2sig = new TGraphAsymmErrors (*plots.getExpLimitGluino2Sig());
+  gluino_xs.xsec2mass (g_exp_2sig);
+
+  TGraph* stop_obs     = new TGraph (*plots.getLimitStop());
+  stop_xs.xsec2mass (stop_obs);
+
+  TGraph* stop_exp      = new TGraph (*plots.getExpLimitStop());
+  stop_xs.xsec2mass (stop_exp);
+  TGraphAsymmErrors* stop_exp_1sig = new TGraphAsymmErrors (*plots.getExpLimitStop1Sig());
+  stop_xs.xsec2mass (stop_exp_1sig);
+  TGraphAsymmErrors* stop_exp_2sig = new TGraphAsymmErrors (*plots.getExpLimitStop2Sig());
+  stop_xs.xsec2mass (stop_exp_2sig);
+
+  
+  TCanvas *canvas = new TCanvas("allMassLifetime", "allMassLifetime", 800, 600);
+  
+  canvas->SetLogx();
+  canvas->SetGridy();
+
+  
+  TH1F* h = new TH1F ("h", "", 1,  7.5e-8, 1e6);
+  h->SetStats (0);
+  h->SetMinimum (300);
+  h->SetMaximum (1300);
+  h->SetTitle("Beamgap Expt");
+  //  h->GetXaxis()->SetTitle("#tau_{#tilde{g},#tilde{t},#tilde{#tau}} [s]");
+  h->GetXaxis()->SetTitle("#tau [s]");
+  h->GetYaxis()->SetTitle("m [GeV]  ");
+  h->Draw ("");
+
+  
+  
+  // limit arrows
+  double* x = g_obs->GetX();
+  for (int i = 0; i < g_obs->GetN(); ++i) {
+    if (x[i] > 0.5) {
+      double y =  g_obs->GetY()[i];
+      TArrow* arrow = new TArrow (x[i], y, h->GetXaxis()->GetXmin(), y, 0.02);
+      arrow->SetLineColor (kRed);
+      arrow->SetLineWidth (2);
+      arrow->Draw();
+      cout << "GLUINO mass limit @ " << x[i] << "sec is found: " << y << endl;
+      break;
+    }
+  }
+  x = g_obs->GetX();
+  for (int i = 0; i < stop_obs->GetN(); ++i) {
+    if (x[i] > 0.5) {
+      double y =  stop_obs->GetY()[i];
+      TArrow* arrow = new TArrow (x[i], y, h->GetXaxis()->GetXmin(), y, 0.02);
+      arrow->SetLineColor (kBlue);
+      arrow->SetLineWidth (2);
+      arrow->Draw();
+      cout << "STOP mass limit @ " << x[i] << "sec is found: " << y << endl;
+      break;
+    }
+  }
+ 
+  // gluino  
+  // 2 sigma band
+  if (g_exp_2sig) {
+    g_exp_2sig->SetLineColor(0);
+    g_exp_2sig->SetLineStyle(0);
+    g_exp_2sig->SetLineWidth(0);
+    g_exp_2sig->SetFillColor(kYellow);
+    g_exp_2sig->SetFillStyle(1001);
+    g_exp_2sig->Draw("3");
+  }
+  
+  // 1 sigma band
+  if (g_exp_1sig) {
+    // g_exp_1sig->SetLineColor(8);
+    g_exp_1sig->SetLineColor(0);
+    g_exp_1sig->SetLineStyle(0);
+    g_exp_1sig->SetLineWidth(0);
+    // g_exp_1sig->SetFillColor(8);
+    g_exp_1sig->SetFillColor(kGreen);
+    g_exp_1sig->SetFillStyle(1001);
+    // g_exp_1sig->SetFillStyle(3005);
+    g_exp_1sig->Draw("3");
+    // g_exp_1sig->Draw("lX");
+  }
+  
+  
+  // epxected limit
+  if (g_exp) {
+    g_exp->SetLineColor(kRed);
+    g_exp->SetLineStyle(4);
+    g_exp->SetLineWidth(2);
+    g_exp->Draw("l3");
+  }
+  
+
+  // observed limit
+  if (g_obs) {
+    g_obs->SetLineColor(kRed);
+    g_obs->SetLineStyle(1);
+    g_obs->SetLineWidth(2);
+    g_obs->Draw("l");
+  }
+  
+  // stop  
+  // 2 sigma band
+  if (stop_exp_2sig) {
+    stop_exp_2sig->SetLineColor(0);
+    stop_exp_2sig->SetLineStyle(0);
+    stop_exp_2sig->SetLineWidth(0);
+    stop_exp_2sig->SetFillColor(kYellow);
+    stop_exp_2sig->SetFillStyle(1001);
+    stop_exp_2sig->Draw("3");
+  }
+  
+  // 1 sigma band
+  if (stop_exp_1sig) {
+    // stop_exp_1sig->SetLineColor(8);
+    stop_exp_1sig->SetLineColor(0);
+    stop_exp_1sig->SetLineStyle(0);
+    stop_exp_1sig->SetLineWidth(0);
+    // stop_exp_1sig->SetFillColor(8);
+    stop_exp_1sig->SetFillColor(kGreen);
+    stop_exp_1sig->SetFillStyle(1001);
+    // stop_exp_1sig->SetFillStyle(3005);
+    stop_exp_1sig->Draw("3");
+    // stop_exp_1sig->Draw("lX");
+  }
+  
+  
+  // epxected limit
+  if (stop_exp) {
+    stop_exp->SetLineColor(kBlue);
+    stop_exp->SetLineStyle(3);
+    stop_exp->SetLineWidth(2);
+    stop_exp->Draw("l3");
+  }
+  
+
+  // observed limit
+  if (stop_obs) {
+    stop_obs->SetLineColor(kBlue);
+    stop_obs->SetLineStyle(2);
+    stop_obs->SetLineWidth(2);
+    stop_obs->Draw("l");
+  }
+  
+  
+
+  TPaveText* blurb = new TPaveText(0.20, 0.63, 0.60, 0.90, "NDC");
+  blurb->AddText("CMS Preliminary 2012");
+  //blurb->AddText("CMS 2012");
+  blurb->AddText("#int L dt = 18.6 fb^{-1}"); //,  #int L_{eff} dt = 935 pb^{-1}");
+  //blurb->AddText("L^{max}_{inst} = 3.5 #times 10^{33} cm^{-2}s^{-1}");
+
+  // std::stringstream label;
+  // label<<"#int L dt = "<<lumi<<" pb^{-1}";
+  // blurb->AddText(label.str().c_str());
+  // double peakInstLumi=maxInstLumi;
+  // int exponent=30;
+  // while (peakInstLumi>10) {
+  //   peakInstLumi/=10;
+  //   ++exponent;
+  // }
+  // std::stringstream label2;
+  // label2<<"L^{max}_{inst} = "<<peakInstLumi<<" x 10^{"<<exponent<<"} cm^{-2}s^{-1}";
+//  blurb->AddText(label2.str().c_str());
+  blurb->AddText("#sqrt{s} = 8 TeV");
+  blurb->AddText("E_{gluon} > 100 GeV, E_{top} > 125 GeV");
+  blurb->SetTextFont(42);
+  blurb->SetBorderSize(0);
+  blurb->SetFillColor(0);
+  blurb->SetShadowColor(0);
+  blurb->SetTextAlign(12);
+  blurb->SetTextSize(0.033);
+  blurb->Draw();
+
+  TLegend* leg = new TLegend(0.6, 0.62, 0.87, 0.90,"95% CL Limits:","NDC");
+  leg->SetTextSize(0.033);
+  leg->SetBorderSize(0);
+  leg->SetTextFont(42);
+  leg->SetFillColor(0);
+
+  leg->AddEntry(g_obs, " #tilde{g} observed", "l");
+  leg->AddEntry(stop_obs, " #tilde{t} observed", "l");
+
+  TGraph* expectedStyle1 = new TGraph (*g_exp);
+  expectedStyle1->SetFillColor (g_exp_1sig->GetFillColor());
+  TGraph* expectedStyle2 = new TGraph (*g_exp);
+  expectedStyle2->SetFillColor (g_exp_2sig->GetFillColor());
+  leg->AddEntry(expectedStyle1, " #tilde{g} expected #pm1#sigma", "lf");
+  leg->AddEntry(expectedStyle2, " #tilde{g} expected #pm2#sigma", "lf");
+
+  expectedStyle1 = new TGraph (*stop_exp);
+  expectedStyle1->SetFillColor (stop_exp_1sig->GetFillColor());
+  expectedStyle2 = new TGraph (*stop_exp);
+  expectedStyle2->SetFillColor (stop_exp_2sig->GetFillColor());
+  leg->AddEntry(expectedStyle1, " #tilde{t} expected #pm1#sigma", "lf");
+  leg->AddEntry(expectedStyle2, " #tilde{t} expected #pm2#sigma", "lf");
+  leg->Draw();
+
+  h->Draw("sameaxis");
+
+  canvas->Print("gluinostopMassLifetime.png");
+  canvas->Print("gluinostopMassLifetime.pdf");
+}
